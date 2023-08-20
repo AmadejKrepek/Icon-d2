47c4ec105343ef07edd015e0db0ee9d2dd27c03f
AmadejKrepek <krepekamadej@gmail.com>
2023-08-20 22:57:27 +0200

    save data

---
+++ 32_super_example/super_dupper_convert_t.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+input_file = "output_data.csv"
+df = pd.read_csv(input_file)
+
+# Filter out data with time equal to 0
+df_filtered = df[df["time"] == 0]
+
+# Remove the "time" column
+df_filtered = df_filtered.drop(columns=["time"])
+
+# Save the filtered DataFrame to a new CSV file
+output_file = "filtered_output_data.csv"
+df_filtered.to_csv(output_file, index=False)
+
+print(f"Filtered data saved to {output_file}")
