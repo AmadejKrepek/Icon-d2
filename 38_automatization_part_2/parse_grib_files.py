f5cbb5eb613ee9ff1bbf181662c8b0b0381415ed
AmadejKrepek <krepekamadej@gmail.com>
2023-08-24 18:08:30 +0200

    created gut files

--- 38_automatization_part_2/parse_grib_files.py
+++ 38_automatization_part_2/parse_grib_files.py
@@ -49,18 +49,19 @@ def crop_dataframe_to_bbox(df, bbox):
     df = df.loc[lat_filter & lon_filter]
     return df
 
-def process_data(filepath, variable, bbox):
+def process_data(filepath, bbox):
     # Load the files with GRIB2 data using pygrib
     grbs = pygrib.open(filepath)
     
     # Retrieve the desired variable
     variable_data = None
+    parameter_name = None
+    
     for grb in grbs:
-        if grb.name == variable:
-            variable_data = grb.values
-            latitudes, longitudes = grb.latlons()
-            valid_date = grb.validDate
-            break
+        variable_data = grb.values
+        latitudes, longitudes = grb.latlons()
+        valid_date = grb.validDate
+        parameter_name = grb.name
     grbs.close()
     
     if variable_data is None:
@@ -70,7 +71,7 @@ def process_data(filepath, variable, bbox):
     df = pd.DataFrame()
     df["Latitude"] = latitudes.ravel()
     df["Longitude"] = longitudes.ravel()
-    df[variable] = variable_data.ravel()
+    df[parameter_name] = variable_data.ravel()
 
     # Perform the bounding box filter
     df = crop_dataframe_to_bbox(df, bbox)
@@ -79,60 +80,62 @@ def process_data(filepath, variable, bbox):
     valid_date = pd.to_datetime(valid_date)
     df["ValidDate"] = valid_date
     
-    return df
+    return df, parameter_name
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Extract weather data within a region")
-    parser.add_argument(
-        "--variable",
-        type=str,
-        default="2 metre temperature",
-        help="The name of the weather data variable to extract",
-    )
-    parser.add_argument(
-        "--source-data",
-        type=str,
-        help="The name of the directory containing the GRIB2 data files",
-        required=True,
-    )
-    parser.add_argument(
-        "--bbox",
-        nargs="+",
-        default=[LAT_MIN, LAT_MAX, LON_MIN, LON_MAX],
-        help="The bounding box used to crop the data, specified as minimum/maximum latitudes and longitudes",
-    )
-    args = parser.parse_args()
-
-    if len(args.bbox) != 4:
-        sys.exit(
-            "The `bbox` argument must contain four values: `minlat maxlat minlon maxlon`"
-        )
-
+    # Directory containing the GRIB2 data files
+    source_data_dir = "./12z"
+    
+    # Create the "output" directory if it doesn't exist
+    output_dir = "output"
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Check if the source data directory exists
+    if not os.path.isdir(source_data_dir):
+        print(f"Source data directory '{source_data_dir}' does not exist. Please provide the correct path.")
+        sys.exit(1)
+    
     # Read all data files in the specified directory
-    filenames = glob.glob(os.path.join(args.source_data, "*.grib2.bz2"))
+    filenames = glob.glob(os.path.join(source_data_dir, "*.grib2.bz2"))
     filenames = sorted(filenames)
 
     output_df = pd.DataFrame()
 
+    parameter_data = {}  # Dictionary to store data for each parameter
+
     for file in filenames:
         print("Processing", file)
         
+        # Decompress the file and read the decompressed data
+        with bz2.BZ2File(file, 'rb') as compressed_file:
+            data = compressed_file.read()
+        # Create a temporary file to hold the decompressed data
+        original_filename = os.path.splitext(os.path.basename(file))[0]
+        temp_decompressed_path = f"{original_filename}_decompressed.grib2"
+        with open(temp_decompressed_path, 'wb') as temp_file:
+            print("Writing decompressed data to", temp_decompressed_path)
+            temp_file.write(data)
+         # Process the data and get parameter name
+        data, parameter_name = process_data(temp_decompressed_path, [LAT_MIN, LAT_MAX, LON_MIN, LON_MAX])
         if data is not None:
+            print("Processing completed. Saving the data...")
             # Convert temperature from Kelvin to Celsius
-            if args.variable == "2 metre temperature":
-                data[args.variable] = kelvin_to_celsius(data[args.variable])
+            data[parameter_name] = kelvin_to_celsius(data[parameter_name])
             
-            # Get date from the data
-            data_date = data["ValidDate"].iloc[0].strftime("%Y-%m-%d")
+            # Check if parameter data exists in the dictionary
+            if parameter_name not in parameter_data:
+                parameter_data[parameter_name] = pd.DataFrame()
             
-            # Create dynamic output folder
-            output_folder = os.path.join("output", args.variable.replace(" ", "_"), data_date)
-            os.makedirs(output_folder, exist_ok=True)
-            
-            # Save the processed data to CSV
-            output_filename = os.path.splitext(os.path.basename(file))[0] + ".csv"
-            output_path = os.path.join(output_folder, output_filename)
-            data.to_csv(output_path, index=False)
+            # Append the data to the parameter's DataFrame
+            parameter_data[parameter_name] = pd.concat([parameter_data[parameter_name], data], ignore_index=True)
+        
+        # Remove the temporary decompressed file
+        os.remove(temp_decompressed_path)
+
+    # Save data for each parameter to separate CSV files
+    for parameter_name, parameter_df in parameter_data.items():
+        print(f"Saving data for parameter '{parameter_name}'...")
+        parameter_df.to_csv(os.path.join("output", parameter_name.replace(" ", "_") + ".csv"), index=False)
 
     print("Script finished.")
