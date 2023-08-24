0180761043fa1c1295c7e5f0c35b660cce1aa611
AmadejKrepek <krepekamadej@gmail.com>
2023-08-24 18:41:10 +0200

    good making

--- 38_automatization_part_2/parse_grib_files.py
+++ 38_automatization_part_2/parse_grib_files.py
@@ -83,59 +83,65 @@ def process_data(filepath, bbox):
     return df, parameter_name
 
 
+def create_output_folders(year, month, day, model_run, parameter_name):
+    output_directory = "output"
+    model_run_dir = os.path.join(output_directory, parameter_name.replace(" ", "_"), year, month, day, model_run + "z")
+    os.makedirs(model_run_dir, exist_ok=True)
+    return model_run_dir
+
+def save_parameter_data(parameter_data, output_directory, year, month, day, model_run):
+    for parameter_name, data_list in parameter_data.items():
+        for data_item in data_list:
+            parameter_df, data_date = data_item
+            model_run_dir = create_output_folders(year, month, day, model_run, parameter_name)
+            output_filename = f"{parameter_name}_{data_date}.csv"
+            output_path = os.path.join(model_run_dir, output_filename)
+            parameter_df.to_csv(output_path, index=False)
+
 if __name__ == "__main__":
-    # Directory containing the GRIB2 data files
     source_data_dir = "./12z"
+    output_directory = "output"
     
-    # Create the "output" directory if it doesn't exist
-    output_dir = "output"
-    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_directory, exist_ok=True)
     
-    # Check if the source data directory exists
     if not os.path.isdir(source_data_dir):
         print(f"Source data directory '{source_data_dir}' does not exist. Please provide the correct path.")
         sys.exit(1)
     
-    # Read all data files in the specified directory
     filenames = glob.glob(os.path.join(source_data_dir, "*.grib2.bz2"))
     filenames = sorted(filenames)
-
-    output_df = pd.DataFrame()
 
     parameter_data = {}  # Dictionary to store data for each parameter
 
     for file in filenames:
         print("Processing", file)
         
-        # Decompress the file and read the decompressed data
         with bz2.BZ2File(file, 'rb') as compressed_file:
             data = compressed_file.read()
-        # Create a temporary file to hold the decompressed data
         original_filename = os.path.splitext(os.path.basename(file))[0]
         temp_decompressed_path = f"{original_filename}_decompressed.grib2"
         with open(temp_decompressed_path, 'wb') as temp_file:
-            print("Writing decompressed data to", temp_decompressed_path)
             temp_file.write(data)
-         # Process the data and get parameter name
+        
         data, parameter_name = process_data(temp_decompressed_path, [LAT_MIN, LAT_MAX, LON_MIN, LON_MAX])
         if data is not None:
-            print("Processing completed. Saving the data...")
-            # Convert temperature from Kelvin to Celsius
             data[parameter_name] = kelvin_to_celsius(data[parameter_name])
             
-            # Check if parameter data exists in the dictionary
-            if parameter_name not in parameter_data:
-                parameter_data[parameter_name] = pd.DataFrame()
+            date_str = original_filename.split("_")[4]
+            year = date_str[:4]
+            month = date_str[4:6]
+            day = date_str[6:8]
+            model_run = date_str[8:]
             
-            # Append the data to the parameter's DataFrame
-            parameter_data[parameter_name] = pd.concat([parameter_data[parameter_name], data], ignore_index=True)
+            if parameter_name not in parameter_data:
+                parameter_data[parameter_name] = []
+            
+            data_date = data["ValidDate"].iloc[0].strftime("%Y-%m-%d")
+            parameter_data[parameter_name].append((data, data_date))
         
-        # Remove the temporary decompressed file
         os.remove(temp_decompressed_path)
 
     # Save data for each parameter to separate CSV files
-    for parameter_name, parameter_df in parameter_data.items():
-        print(f"Saving data for parameter '{parameter_name}'...")
-        parameter_df.to_csv(os.path.join("output", parameter_name.replace(" ", "_") + ".csv"), index=False)
+    save_parameter_data(parameter_data, output_directory, year, month, day, model_run)
 
     print("Script finished.")
