82f4d475beae7d91be05aeea2ac4f3e1d577572a
AmadejKrepek <krepekamadej@gmail.com>
2023-08-25 19:03:22 +0200

    succesfully generated maps

--- 39_beta_test_generating_maps/generate_maps.py
+++ 39_beta_test_generating_maps/generate_maps.py
@@ -1,24 +1,33 @@
 import pandas as pd
-import geopandas as gpd
+import os
+from datetime import datetime
 import matplotlib.pyplot as plt
-from shapely.geometry import Polygon, Point
 import numpy as np
+import geopandas as gpd
 import matplotlib.colors as mcolors
 import matplotlib.image as mpimg
 from scipy.ndimage import zoom
-import matplotlib.patches as patches
+from shapely.geometry import Polygon, Point
 from matplotlib.font_manager import FontProperties
-import os
-from datetime import datetime
 
-# Define your custom colormap creation functions
-
-
-def create_custom_colormap(colors, cmap_name, N):
-    return mcolors.LinearSegmentedColormap.from_list(cmap_name, colors, N=N)
-
-
-def create_variable_plot(df, variable_name, title, x_title, colormap, legend_ticks, value_range, output_filename, model_run_formatted_date, model_run, selected_formatted_date):
+# Function to create the plot
+def create_variable_plot(
+    df, 
+    variable_name, 
+    title,
+    x_title, 
+    colormap, 
+    legend_ticks, 
+    value_range, 
+    output_filename,
+    model_run_formatted_date,
+    model_run,
+    selected_formatted_date,
+    custom_font):
+    if variable_name not in df.columns:
+        print(f"Variable '{variable_name}' not found in the CSV file. Skipping...")
+        return
+    
     # Read the variable data
     variable_values = df.pivot("Latitude", "Longitude", variable_name).values
 
@@ -26,8 +35,7 @@ def create_variable_plot(df, variable_name, title, x_title, colormap, legend_tic
     lon_values = df['Longitude'].unique()
 
     # Load the world map with medium resolution
-    world = gpd.read_file(
-        '../data/shapes/shape/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
+    world = gpd.read_file('../data/shapes/shape/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
 
     # Define the neighboring countries
     countries = ['Slovenia', 'Austria', 'Italy', 'Hungary', 'Croatia']
@@ -41,44 +49,35 @@ def create_variable_plot(df, variable_name, title, x_title, colormap, legend_tic
     LON_MAX = 16.7955
 
     # Create bounding box for the region
-    bbox_polygon = Polygon(
-        [(LON_MIN, LAT_MIN), (LON_MIN, LAT_MAX), (LON_MAX, LAT_MAX), (LON_MAX, LAT_MIN)])
+    bbox_polygon = Polygon([(LON_MIN, LAT_MIN), (LON_MIN, LAT_MAX), (LON_MAX, LAT_MAX), (LON_MAX, LAT_MIN)])
 
     # Adjust the bounding box of the region to match the defined bounding box
     region_clipped = gpd.clip(region, bbox_polygon)
 
     # Plot the bounding areas with detailed geometries
-    fig, ax = plt.subplots(figsize=(15, 15))
-
+    fig, ax = plt.subplots(figsize=(15, 15))    
+    
     # Adjust the position of the axes to control left padding
-    gpd.GeoSeries(bbox_polygon).boundary.plot(
-        ax=ax, color='#333333', linestyle='--')
+    gpd.GeoSeries(bbox_polygon).boundary.plot(ax=ax, color='#333333', linestyle='--')
     fig.set_facecolor('#333333')
 
     # Clip variable values within the desired range
     if variable_name == "Total_Precipitation":
-        variable_clipped = np.clip(
-            variable_values, value_range[0], value_range[1])
-        contour_levels = precip_contour_levels
+        variable_clipped = np.clip(variable_values, value_range[0], value_range[1])
+        contour_levels = np.arange(legend_ticks[0], legend_ticks[-1] + 1, step=10)
     elif variable_name == "windgusts_10m_max":
-        variable_clipped = np.clip(
-            variable_values, value_range[0], value_range[1])
-        contour_levels = np.arange(
-            legend_ticks[0], legend_ticks[-1] + 1, step=10)
+        variable_clipped = np.clip(variable_values, value_range[0], value_range[1])
+        contour_levels = np.arange(legend_ticks[0], legend_ticks[-1] + 1, step=10)
     else:
-        variable_clipped = np.clip(
-            variable_values, value_range[0], value_range[1])
-        contour_levels = np.arange(
-            legend_ticks[0], legend_ticks[-1] + 2, step=2)
+        variable_clipped = np.clip(variable_values, value_range[0], value_range[1])
+        contour_levels = np.arange(legend_ticks[0], legend_ticks[-1] + 2, step=2)
 
     # Identify the maximum variable value and its coordinates
     max_value = variable_clipped.max()
-    max_value_coords = np.unravel_index(
-        variable_clipped.argmax(), variable_clipped.shape)
+    max_value_coords = np.unravel_index(variable_clipped.argmax(), variable_clipped.shape)
 
     # Use the specified colormap in the contour plot
-    contour_plot = ax.contourf(lon_values, lat_values, variable_clipped,
-                               levels=contour_levels, cmap=colormap, extend='both')
+    contour_plot = ax.contourf(lon_values, lat_values, variable_clipped, levels=contour_levels, cmap=colormap, extend='both')
 
     # Optionally, set stride to control the density of the text
     stride = 4
@@ -93,8 +92,7 @@ def create_variable_plot(df, variable_name, title, x_title, colormap, legend_tic
             var_val = int(round(variable_clipped[i, j]))
             # Always plot the maximum value
             if (i, j) == max_value_coords or var_val != 0:
-                ax.text(lon_values[j], lat_values[i],
-                        f'{var_val}', fontsize=8, ha='center', va='center', color='black')
+                ax.text(lon_values[j], lat_values[i], f'{var_val}', fontsize=8, ha='center', va='center', color='black')
 
     # Add borders between countries
     region_clipped.boundary.plot(ax=ax, linewidth=2, color='#333333')
@@ -124,66 +122,49 @@ def create_variable_plot(df, variable_name, title, x_title, colormap, legend_tic
     logo_resized = zoom(logo, (scaling_factor, scaling_factor, 1))
 
     fig.figimage(logo_resized, xo=60, yo=3110, zorder=20)
-
-    title_font = {'family': custom_font.get_name(), 'size': '15',
-                  'color': 'white', 'weight': 'bold'}
-    subtitle_font = {'family': custom_font.get_name(), 'size': '11',
-                     'color': 'white'}
-
+        
+    title_font = {'family' : custom_font.get_name(), 'size':'15', 'color':'white', 'weight':'bold'}
+    subtitle_font = {'family' : custom_font.get_name(), 'size':'11', 'color':'white'}
+    
     header_padding = 0.01
     padding = 0.02
-
+    
     # Invisible text to adjust the padding
     fig.text(0.5, 0.795 + padding, 'Upper', ha='center', **title_font, alpha=0)
-    fig.text(0.904, 0.12 + padding, 'Left',
-             ha="right", **subtitle_font, alpha=0)
-    fig.text(0.127, 0.12, "Napovedni model: ICON-D2 15z",
-             ha="left", **subtitle_font, alpha=0)
-    fig.text(0.899, 0.12, "Vir podatkov: Open-Meteo",
-             ha="right", **subtitle_font, alpha=0)
-
+    fig.text(0.904, 0.12 + padding, 'Left', ha="right", **subtitle_font, alpha=0)
+    fig.text(0.127, 0.12, "Napovedni model: ICON-D2 15z", ha="left", **subtitle_font, alpha=0)
+    fig.text(0.899, 0.12, "Vir podatkov: Open-Meteo", ha="right", **subtitle_font, alpha=0)
+    
     # Set title and source information
-    fig.text(0.5, 0.795 + header_padding,
-             f'{selected_formatted_date}', ha='center', **title_font)
+    fig.text(0.5, 0.795 + header_padding, f'{selected_formatted_date}', ha='center', **title_font)
     fig.text(0.897, 0.125, "vir podatkov: DWD", ha="right", **subtitle_font)
-    fig.text(0.127, 0.125,
-             f'ICON-D2 ({model_run_formatted_date} {model_run} UTC)', ha="left", **subtitle_font)
+    fig.text(0.127, 0.125, f'ICON-D2 ({model_run_formatted_date} {model_run} UTC)', ha="left", **subtitle_font)
     fig.text(0.897, 0.795 + header_padding, title, ha='right', **title_font)
 
     cax_height = 0.02
 
     # Create axes for the colorbar to make it the same width as the plot, and place at the very bottom
     cax = fig.add_axes([0.15, 0.17, 0.7, cax_height])
-
-    cbar = plt.colorbar(contour_plot, cax=cax,
-                        orientation='horizontal', ticks=legend_ticks, label=x_title)
-
+    
+    cbar = plt.colorbar(contour_plot, cax=cax, orientation='horizontal', ticks=legend_ticks, label=x_title)
+    
     # Adjust the width of the colorbar
-    cbar.ax.set_position([cax.get_position().x0 - 0.025, cax.get_position().y0 - 0,
-                         cax.get_position().width + 0.075, cax.get_position().height])
-
+    cbar.ax.set_position([cax.get_position().x0 - 0.025, cax.get_position().y0 - 0, cax.get_position().width + 0.075, cax.get_position().height])
+    
     # Set the colorbar tick label color
     cbar.ax.xaxis.set_tick_params(color='white')
     # Set the color of the tick labels
-    cbar.set_ticklabels([str(int(level))
-                        for level in legend_ticks], color='white')
+    cbar.set_ticklabels([str(int(level)) for level in legend_ticks], color='white')
 
     # Set the colorbar label color
-    cbar.set_label(x_title, color='white', labelpad=11,
-                   fontproperties=custom_font)
-
+    cbar.set_label(x_title, color='white', labelpad=11, fontproperties=custom_font)
+    
     # Set edgecolor of colorbar to 'none' to remove the border
     cbar.outline.set_edgecolor('none')
 
-    # Format the tick labels
-    if variable_name == "Total_Precipitation":
-        cax.set_xticks(precip_contour_levels)
-        cax.set_xticklabels([str(int(level))
-                            for level in precip_contour_levels])
-    else:
-        step = 5
-        cax.set_xticks(contour_levels)
-        cax.set_xticklabels([str(int(level)) for level in contour_levels])
+    step = 5
+    cax.set_xticks(contour_levels)
+    cax.set_xticklabels([str(int(level)) for level in contour_levels])
 
     # Make the border white and add padding
     for spine in ax.spines.values():
@@ -195,25 +176,5 @@ def create_variable_plot(df, variable_name, title, x_title, colormap, legend_tic
     ax.tick_params(axis='y', colors='white')
 
     # Save the figure with adjusted left padding
-    # Adjust the pad_inches value as needed
-    plt.savefig(output_filename, dpi=300, bbox_inches='tight')
+    plt.savefig(output_filename, dpi=300, bbox_inches='tight')  # Adjust the pad_inches value as needed
     plt.close()
-
-# Define a function to create and save the plots for different variables
-
-
-def generate_plots(variables_and_settings, input_filename, output_directory):
-    df = pd.read_csv(input_filename)
-
-    for variable, title, x_title, colormap, legend_ticks, value_range in variables_and_settings:
-        if variable in df.columns:
-            output_variable_name = variable.replace(' ', '_').lower()
-            output_filename = f'{output_variable_name}.png'
-            output_filepath = os.path.join(output_directory, output_filename)
-
-            # Call your create_variable_plot function to generate the plot
-            create_variable_plot(df, variable, title, x_title, colormap, legend_ticks, value_range,
-                                 output_filepath, model_run_formatted_date, model_run, selected_formatted_date)
-        else:
-            print(f"Variable '{variable}' not found in the CSV file. Skipping...")
-
--- 39_beta_test_generating_maps/generate_maps_guide.py
+++ 39_beta_test_generating_maps/generate_maps_guide.py
@@ -1,14 +1,131 @@
-from generate_maps import generate_plots
+from generate_maps import create_variable_plot
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, Point
+import numpy as np
+import matplotlib.colors as mcolors
+import matplotlib.image as mpimg
+from scipy.ndimage import zoom
+import matplotlib.patches as patches
+from matplotlib.font_manager import FontProperties
+import os
+from datetime import datetime
 
-# Define your input and output directories
-input_directory = '../38_automatization_part_2/data/output/2_metre_temperature/max/2023/08/25/12z/'
-output_directory = '../38_automatization_part_2/data/public/plots/'
+# Enhanced colormap with more detailed gradient
+colors = ["#f2f2f2","#b5c9fd","#9fbefd","#889eea","#6171f7","#3e55f4","#009694","#0cff00","#e6ff00","#ffff00","#ffcf00","#ff9f00","#ff6f00","#ff1b00","#e60000","#cc0000","#a600a4","#c27ec1","#e094c3","#ffbffd","#f5a6f9","#6fc3fb","#0098fe"]
+cmap_custom = mcolors.LinearSegmentedColormap.from_list('custom_cmap', colors, N=300)
 
-# Define the variables and settings
+temperature_colors_1 = [
+    "#ff89ff","#a774ff","#27b4ff","#88e4ff","#56c976","#bae972","#ffff6f","#fda450","#ff4941","#8c3232"
+]
+
+temperature_colors_2 = [
+    "#dba1cf","#c983c1","#a779ba","#896db5","#6d66ad","#6a77c8","#5b8cd9","#4ba7f1","#56bbfe",
+    "#6cc9fe","#85daff","#6094a0","#74ad83","#75bd6c","#a0c969","#cad778","#e7ea7f","#fff683",
+    "#fef9ce","#fee4a6","#fed67c","#febb5b","#fea24f","#f88438","#f36a36","#e24f2c","#e13027",
+    "#bf2b25","#962727","#a03937","#b55757","#ba8080"
+]
+
+temperature_colors = [
+    "#dba1cf","#c180bc","#a779ba","#896db5","#6d66ad","#6c7bc4","#6091d8","#4ba7f1","#56bbfe",
+    "#6cc9fe","#84d4fe","#6199a2","#74ad83","#75bd6c","#a0c969","#cad778","#e7ea7f","#fff683",
+    "#fef9ce","#fee4a6","#fed67c","#febb5b","#fea24f","#f88438","#f36a36","#e24f2c","#e13027",
+    "#bf2b25","#962727","#a03937","#b55757","#ba8080"
+]
+
+wind_max_2m_colors = [
+    '#FFFFFF',                          # White (0 km/h)
+    '#00FF00', '#00E600', '#00CC00',   # Green (0-50 km/h)
+    '#FFFF00', '#FFCC00',              # Yellow (50-100 km/h)
+    '#FFA500', '#FF8C00',              # Orange (100-150 km/h)
+    '#FF0000', '#CC0000',              # Red (150-200 km/h)
+    '#FFFF00'                          # Yellow (>200 km/h)
+]
+
+cmap_temperature = mcolors.LinearSegmentedColormap.from_list('temperature_cmap', temperature_colors, N=500)
+cmap_wind_max_2m_colors = mcolors.LinearSegmentedColormap.from_list('wind_max_2m_cmap', wind_max_2m_colors, N=300)
+
+# Define custom tick labels for precipitation
+precip_ticks = [0, 1, 2, 5, 10, 15, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 250, 300, 350]
+
+# Define the specific contour levels for precipitation
+precip_contour_levels = [0, 1, 2, 5, 10, 15, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 250, 300, 350]
+
+# Specify the path to the directory containing the custom font file
+font_path = '../assets/fonts/'
+
+# Register the custom font using FontProperties
+custom_font = FontProperties(fname=font_path + 'font.ttf')
+
+# List of variable names, titles, colormaps, legend ticks, and value ranges
 variables_and_settings = [
     ("2 metre temperature", "najvišja temperatura zraka", "temperatura [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
     ("2 metre dewpoint temperature", "najvišja temperatura rosišča", "temperatura rosišča [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+    ("temperature_2m_min", "Minimalna temperatura zraka na višini 2 m", "Temperatura zraka [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+    ("windgusts_10m_max", "Maksimalni sunki vetra na višini 10 m", "Sunki vetra [km/h]", cmap_wind_max_2m_colors, list(range(0, 200, 10)), (0, 200)),
+    ("Total_Precipitation", "Napoved količine padavin", "Količina padavin [mm]",  cmap_custom, precip_ticks, (0, 350)),
 ]
 
-# Call the generate_plots function to generate and save the plots
-generate_plots(variables_and_settings, input_directory, output_directory)
+# Loop through the variables and create plots
+for variable, title, x_title, colormap, legend_ticks, value_range in variables_and_settings:
+    input_filename = f'../38_automatization_part_2/data/output/2_metre_temperature/max/2023/08/25/15z/max_2_metre_temperature_2023_08_26_15_2023_08_26_00_00_2023_08_26_23_00.csv'
+    
+    df = pd.read_csv(input_filename)
+    if variable not in df.columns:
+        print(f"Variable '{variable}' not found in the CSV file. Skipping...")
+    else: 
+        # Extract relevant parts from the input filename
+        parts = input_filename.split('/')
+        model_run_year = parts[-5]
+        model_run_month = parts[-4]
+        model_run_day = parts[-3]
+        model_run = parts[-2]
+        output_variable_name = variable.replace(' ', '_').lower()
+        
+        selected_file_name = parts[-1]
+        selected_file_name_parts = selected_file_name.split('_')
+        print(selected_file_name_parts)
+        
+        selected_aggregate = selected_file_name_parts[0]
+        selected_parameter = selected_file_name_parts[1:3]
+        
+        selected_year = selected_file_name_parts[4]
+        selected_month = selected_file_name_parts[5]
+        selected_day = selected_file_name_parts[6]
+        
+        selected_start_year = selected_file_name_parts[8]
+        selected_start_month = selected_file_name_parts[9]
+        selected_start_day = selected_file_name_parts[10]
+        selected_start_hour = selected_file_name_parts[11]
+        selected_start_minute = selected_file_name_parts[12]
+        
+        selected_end_year = selected_file_name_parts[13]
+        selected_end_month = selected_file_name_parts[14]
+        selected_end_day = selected_file_name_parts[15]
+        selected_end_hour = selected_file_name_parts[16]
+        selected_end_minute = selected_file_name_parts[17]
+        
+        formatted_start_datetime = "_".join([selected_start_year, selected_start_month, selected_start_day, selected_start_hour, selected_start_minute])
+        formatted_end_datetime = "_".join([selected_end_year, selected_end_month, selected_end_day, selected_end_hour, selected_end_minute])
+
+        # Parse the date components
+        model_run_date_components = datetime.strptime(f"{model_run_year}-{model_run_month}-{model_run_day}", "%Y-%m-%d")
+        model_run_formatted_date = model_run_date_components.strftime("%d. %m. %Y")
+        
+        selected_date_components = datetime.strptime(f"{selected_year}-{selected_month}-{selected_day}", "%Y-%m-%d")
+        selected_formatted_date = selected_date_components.strftime("%d. %m. %Y")
+        
+        # Construct the output directory and filename
+        output_directory = f'../38_automatization_part_2/data/public/plots/{output_variable_name}/{selected_aggregate}/{model_run_year}/{model_run_month}/{model_run_day}/{model_run}/'
+        print(f"Output directory: {output_directory}")
+        os.makedirs(output_directory, exist_ok=True)  # Create the output directory if it doesn't exist
+        
+        model_run = model_run.replace('z', '')
+        
+        output_filename = f'{selected_aggregate}_{output_variable_name}_{selected_year}_{selected_month}_{selected_day}_{model_run}_{formatted_start_datetime}_{formatted_end_datetime}.png'
+        output_filepath = os.path.join(output_directory, output_filename)
+        create_variable_plot(df, variable, title, x_title, colormap, legend_ticks, value_range, output_filepath, model_run_formatted_date, model_run, selected_formatted_date, custom_font)
+
+
+
