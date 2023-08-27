a02a73bdf18e909cb006b46e90c1923de71fa366
AmadejKrepek <krepekamadej@gmail.com>
2023-08-27 19:14:15 +0200

    added fgor move changes

---
+++ 41_automatization_part_4/generate_maps/general/create_dynamic_maps.py
@@ -0,0 +1,82 @@
+from .generate_maps import create_variable_plot, extract_output_names
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, Point
+import numpy as np
+import matplotlib.colors as mcolors
+import matplotlib.image as mpimg
+from scipy.ndimage import zoom
+import matplotlib.patches as patches
+from matplotlib.font_manager import FontProperties
+import os
+from datetime import datetime
+
+def create_maps(input_filename, output_directory):
+    # Enhanced colormap with more detailed gradient
+    colors = ["#f2f2f2","#b5c9fd","#9fbefd","#889eea","#6171f7","#3e55f4","#009694","#0cff00","#e6ff00","#ffff00","#ffcf00","#ff9f00","#ff6f00","#ff1b00","#e60000","#cc0000","#a600a4","#c27ec1","#e094c3","#ffbffd","#f5a6f9","#6fc3fb","#0098fe"]
+    cmap_custom = mcolors.LinearSegmentedColormap.from_list('custom_cmap', colors, N=300)
+
+    temperature_colors_1 = [
+        "#ff89ff","#a774ff","#27b4ff","#88e4ff","#56c976","#bae972","#ffff6f","#fda450","#ff4941","#8c3232"
+    ]
+
+    temperature_colors_2 = [
+        "#dba1cf","#c983c1","#a779ba","#896db5","#6d66ad","#6a77c8","#5b8cd9","#4ba7f1","#56bbfe",
+        "#6cc9fe","#85daff","#6094a0","#74ad83","#75bd6c","#a0c969","#cad778","#e7ea7f","#fff683",
+        "#fef9ce","#fee4a6","#fed67c","#febb5b","#fea24f","#f88438","#f36a36","#e24f2c","#e13027",
+        "#bf2b25","#962727","#a03937","#b55757","#ba8080"
+    ]
+
+    temperature_colors = [
+        "#dba1cf","#c180bc","#a779ba","#896db5","#6d66ad","#6c7bc4","#6091d8","#4ba7f1","#56bbfe",
+        "#6cc9fe","#84d4fe","#6199a2","#74ad83","#75bd6c","#a0c969","#cad778","#e7ea7f","#fff683",
+        "#fef9ce","#fee4a6","#fed67c","#febb5b","#fea24f","#f88438","#f36a36","#e24f2c","#e13027",
+        "#bf2b25","#962727","#a03937","#b55757","#ba8080"
+    ]
+
+    wind_max_2m_colors = [
+        '#FFFFFF',                          # White (0 km/h)
+        '#00FF00', '#00E600', '#00CC00',   # Green (0-50 km/h)
+        '#FFFF00', '#FFCC00',              # Yellow (50-100 km/h)
+        '#FFA500', '#FF8C00',              # Orange (100-150 km/h)
+        '#FF0000', '#CC0000',              # Red (150-200 km/h)
+        '#FFFF00'                          # Yellow (>200 km/h)
+    ]
+
+    cmap_temperature = mcolors.LinearSegmentedColormap.from_list('temperature_cmap', temperature_colors, N=500)
+    cmap_wind_max_2m_colors = mcolors.LinearSegmentedColormap.from_list('wind_max_2m_cmap', wind_max_2m_colors, N=300)
+
+    # Define custom tick labels for precipitation
+    precip_ticks = [0, 1, 2, 5, 10, 15, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 250, 300, 350]
+
+    # Define the specific contour levels for precipitation
+    precip_contour_levels = [0, 1, 2, 5, 10, 15, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 250, 300, 350]
+
+    # Specify the path to the directory containing the custom font file
+    font_path = '../assets/fonts/'
+
+    # Register the custom font using FontProperties
+    custom_font = FontProperties(fname=font_path + 'font.ttf')
+
+    # List of variable names, titles, colormaps, legend ticks, and value ranges
+    variables_and_settings = [
+        ("max 2 metre temperature", "najvišja temperatura zraka", "temperatura [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+        ("min 2 metre temperature", "najnižja temperatura zraka", "temperatura [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+        ("max 2 metre dewpoint temperature", "najvišja temperatura rosišča", "temperatura rosišča [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+        ("min 2 metre dewpoint temperature", "najnižja temperatura rosišča", "temperatura rosišča [°C]", cmap_temperature, list(range(-20, 40, 1)), (-20, 40)),
+        ("windgusts_10m_max", "Maksimalni sunki vetra na višini 10 m", "Sunki vetra [km/h]", cmap_wind_max_2m_colors, list(range(0, 200, 10)), (0, 200)),
+        ("Total_Precipitation", "Napoved količine padavin", "Količina padavin [mm]",  cmap_custom, precip_ticks, (0, 350)),
+    ]
+
+    # Loop through the variables and create plots
+    for variable, title, x_title, colormap, legend_ticks, value_range in variables_and_settings:        
+        df = pd.read_csv(input_filename)
+        if variable not in df.columns:
+            print(f"Variable '{variable}' not found in the CSV file. Skipping...")
+        else: 
+            output_filepath, model_run_formatted_date, model_run, selected_formatted_date = extract_output_names(input_filename, variable, output_directory)
+            create_variable_plot(df, variable, title, x_title, colormap, legend_ticks, value_range, output_filepath, model_run_formatted_date, model_run, selected_formatted_date, custom_font, precip_contour_levels)
+
+
+
---
+++ 42_fix_latest_grib_run/choose_parameters.py
@@ -0,0 +1,51 @@
+from test import get_latest_model_run_filename, download_and_extract_log_file
+from datetime import datetime
+
+def read_variable_names_from_file(file_path):
+    with open(file_path, "r") as f:
+        variable_names = [line.strip() for line in f if line.strip()]
+    return variable_names
+
+def getGribFileNames():
+    print("Script started at:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    variable_names_file = "./variable_names/d2_variables.txt"
+    variable_names = read_variable_names_from_file(variable_names_file)
+    data = download_and_extract_log_file()
+
+    print("Available parameters:")
+    for idx, param in enumerate(variable_names, start=1):
+        print(f"{idx}. {param}")
+
+    parameter_input = input("Enter parameter numbers separated by commas: ")
+    selected_indices = [int(idx.strip()) for idx in parameter_input.split(",")]
+
+    selected_params = []
+    for idx in selected_indices:
+        if 1 <= idx <= len(variable_names):
+            selected_params.append(variable_names[idx - 1])
+        else:
+            print(f"Invalid index {idx}. Skipping.")
+
+    if not selected_params:
+        print("No valid parameters selected. Exiting.")
+        return
+
+    print("Searching for model runs...")
+    filenames = []
+    for param in selected_params:
+        latest_file = get_latest_model_run_filename(data, param)
+        if latest_file:
+            filenames.append(latest_file)
+            print(f"Latest model run filename for parameter '{param}': {latest_file}")
+        else:
+            print(f"No regular-lat-lon model run found for parameter '{param}'.")
+
+    if not filenames:
+        print("No filenames available for the selected parameters.")
+
+    print("Script finished at:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    
+    return filenames
+
+getGribFileNames()
