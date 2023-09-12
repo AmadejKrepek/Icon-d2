b11bc39aa92901f19e7b402a4b3154ab732868e5
AmadejKrepek <krepekamadej@gmail.com>
2023-09-12 16:28:18 +0200

    added more simple approach

--- 49_automatization_part_7/generate_maps/generate_maps_guide.py
+++ 49_automatization_part_7/generate_maps/generate_maps_guide.py
@@ -19,28 +19,15 @@ def list_and_select(base_path, item_type):
 def generate_fancy_maps(base_path, output_directory, color_configuration, custom_font):
     current_path = base_path
     selected_items = []
-    provider_directory = None
-    model_directory = None
 
     while True:
         selected_item = list_and_select(current_path, "item")
         selected_items.append(selected_item)
-
-        # Check if we've reached the end of the directory structure
-        if os.path.isfile(os.path.join(current_path, selected_item)):
+        
+        if selected_item.endswith(".csv"):
             break
-
-        # Check if the selected item is the provider or model directory
-        if selected_item in ["DWD", "ARSO"]:  # Adjust these values to match your provider names
-            provider_directory = selected_item
-        elif selected_item in ["IconD2", "Aladin"]:  # Adjust these values to match your model names
-            model_directory = selected_item
-
+        
         current_path = os.path.join(current_path, selected_item)
-
-    # Combine the provider and model directories with the output_directory
-    if provider_directory and model_directory:
-        output_directory = os.path.join(output_directory, provider_directory, model_directory)
 
     # Construct input_filename based on user selections
     input_filename = construct_input_filename(base_path, *selected_items)
