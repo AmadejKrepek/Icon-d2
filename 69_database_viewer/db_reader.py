49eb3bbadacadc77ed35bc1355421f76d52708f5
Amadej Krepek <krepekamadej@gmail.com>
2023-09-23 16:53:25 +0200

    craeted my own logic

--- 69_database_viewer/db_reader.py
+++ 69_database_viewer/db_reader.py
@@ -1,31 +1,142 @@
-from datetime import timedelta
-import psycopg2
 import csv
-from dotenv import load_dotenv
-import os
-import traceback
 
-# Load environment variables from .env
-load_dotenv()
-
-# Access the environment variables
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-def get_table_list():
+def select_record_and_aggregate(cursor, table_name):
     try:
-        # Establish a connection to the PostgreSQL database
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            database=DB_NAME,
-            user=DB_USERNAME,
-            password=DB_PASSWORD
-        )
+        # Query to retrieve the rows from the selected table
+        cursor.execute(f"SELECT DISTINCT start_date, model_run FROM {table_name}")
 
+        # Fetch the available records and model runs
+        records_and_model_runs = cursor.fetchall()
+
+        if not records_and_model_runs:
+            print(f"No records found in table '{table_name}'.")
+            return
+
+        # Display available records and model runs
+        print("Available records and model runs:")
+        for i, (start_date, model_run) in enumerate(records_and_model_runs, start=1):
+            print(f"{i}. Start Date: {start_date}, Model Run: {model_run}")
+
+        try:
+            record_choice = int(input("Enter the number of the record you want to choose: ")) - 1
+            if 0 <= record_choice < len(records_and_model_runs):
+                selected_record = records_and_model_runs[record_choice]
+            else:
+                print("Invalid selection.")
+                return
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            return
+
+        # Ask the user to select a model run
+        model_runs = [record[1] for record in records_and_model_runs if record[0] == selected_record[0]]
+        print("Available model runs for the selected record:")
+        for i, model_run in enumerate(model_runs, start=1):
+            print(f"{i}. Model Run: {model_run}")
+
+        try:
+            model_run_choice = int(input("Enter the number of the model run you want to choose: ")) - 1
+            if 0 <= model_run_choice < len(model_runs):
+                selected_model_run = model_runs[model_run_choice]
+            else:
+                print("Invalid selection.")
+                return
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            return
+
+        # Ask the user to select an aggregation method
+        print("Available aggregations:")
+        print("1. Max")
+        print("2. Min")
+        print("3. Average")
+
+        try:
+            aggregation_choice = int(input("Enter the number of the aggregation you want to perform: "))
+            if aggregation_choice in [1, 2, 3]:
+                selected_aggregation = aggregation_choice
+            else:
+                print("Invalid selection.")
+                return
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            return
+
+        # Aggregate the data based on the selected record, model run, and aggregation choice
+        aggregated_data = aggregate_data(cursor, table_name, selected_record[0], selected_model_run, selected_aggregation)
+
+        if not aggregated_data:
+            return
+
+        # Write aggregated data to CSV file
+        write_aggregated_data_to_csv(selected_record[0], selected_model_run, selected_aggregation, aggregated_data)
+
+    except Exception as e:
+        # Log the error and display a more informative message
+        error_message = f"An error occurred: {str(e)}"
+        print(error_message)
+
+def extract_data(aggregated_data):
+    start_date = aggregated_data[0][0]
+    end_date = aggregated_data[0][1]
+    interval = aggregated_data[0][2]
+    print(len(aggregated_data[0][3]))
+    for item in aggregated_data[0][3]:
+        #print(item)
+        break;
+
+def aggregate_data(cursor, table_name, selected_record, selected_model_run, aggregation_choice):
+    try:
+        aggregation_sql = ""
+        if aggregation_choice == 1:
+            aggregation_sql = f"MAX(data) AS aggregated_data"
+        elif aggregation_choice == 2:
+            aggregation_sql = f"MIN(data) AS aggregated_data"
+        elif aggregation_choice == 3:
+            aggregation_sql = f"AVG(data) AS aggregated_data"
+
+        # Modify the SQL query to include a GROUP BY clause
+        cursor.execute(f"""
+            SELECT start_date, end_date, interval, {aggregation_sql}
+            FROM {table_name}
+            WHERE start_date = %s AND model_run = %s
+            GROUP BY start_date, end_date, interval
+        """, (selected_record, selected_model_run))
+
+        # Fetch aggregated data
+        aggregated_data = cursor.fetchall()
+
+        aggregated_data = extract_data(aggregated_data)
+
+        return aggregated_data
+
+    except Exception as e:
+        # Log the error and display a more informative message
+        error_message = f"An error occurred while aggregating data: {str(e)}"
+        print(error_message)
+        return None
+
+def write_aggregated_data_to_csv(selected_record, selected_model_run, selected_aggregation, aggregated_data):
+    try:
+        # Prepare data for CSV
+        csv_data = [[start_date, end_date, aggregated_value] for start_date, end_date, aggregated_value in aggregated_data]
+
+        # Write data to CSV file
+        output_file = f"aggregated_data_{selected_record}_{selected_model_run}_{selected_aggregation}.csv"
+        with open(output_file, "w", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["Start Date", "End Date", "Aggregated Value"])  # CSV header
+            csv_writer.writerows(csv_data)
+
+        print(f"CSV file '{output_file}' created successfully.")
+
+    except Exception as e:
+        # Log the error and display a more informative message
+        error_message = f"An error occurred while writing data to CSV: {str(e)}"
+        print(error_message)
+
+def get_table_list(conn):
+    try:
         # Create a cursor object
         cursor = conn.cursor()
 
@@ -39,9 +150,8 @@ def get_table_list():
         # Fetch all table names
         table_names = cursor.fetchall()
 
-        # Close the cursor and the connection
+        # Close the cursor
         cursor.close()
-        conn.close()
 
         return [row[0] for row in table_names]
 
@@ -51,8 +161,8 @@ def get_table_list():
         print(error_message)
         return []
 
-def select_table():
-    table_names = get_table_list()
+def select_table(conn):
+    table_names = get_table_list(conn)
     if not table_names:
         print("No tables found in the database.")
         return
@@ -72,120 +182,4 @@ def select_table():
         print("Invalid input. Please enter a valid number.")
         return None
 
-def aggregate_data_by_date(data, selected_date, aggregation_func=max):
-    try:
-        # Filter data for the selected date
-        filtered_data = [row for row in data if row[0].date() == selected_date]
 
-        if not filtered_data:
-            print(f"No data found for the selected date: {selected_date}.")
-            return None
-
-        # Extract the values to be aggregated
-        values_to_aggregate = [row[3] for row in filtered_data]
-
-        if not values_to_aggregate:
-            print(f"No values found for aggregation on date: {selected_date}.")
-            return None
-
-        # Initialize a list to store aggregated values
-        aggregated_values = []
-
-        # Iterate through the coordinates and aggregate element-wise
-        for i in range(len(values_to_aggregate[0])):
-            values_at_index = [nested_array[i] if nested_array[i] is not None else 0.0 for nested_array in values_to_aggregate[0]]
-            print(values_at_index)
-            aggregated_value = aggregation_func(values_at_index)
-            aggregated_values.append(aggregated_value)
-
-        return aggregated_values
-
-    except Exception as e:
-        # Log the error and display a more informative message
-        error_message = f"An error occurred while aggregating data: {str(e)}"
-        print(error_message)
-        return None
-
-
-
-def write_aggregated_data_to_csv(selected_date, aggregated_values, output_file):
-    try:
-        # Prepare data for CSV
-        data_to_write = [selected_date] + aggregated_values
-
-        # Write data to CSV file
-        with open(output_file, "w", newline="") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(["Date"] + [f"Aggregated_Value_{i+1}" for i in range(len(aggregated_values))])  # CSV header
-            csv_writer.writerow(data_to_write)
-
-        print(f"CSV file '{output_file}' created successfully.")
-
-    except Exception as e:
-        # Log the error and display a more informative message
-        error_message = f"An error occurred while writing data to CSV: {str(e)}"
-        print(error_message)
-
-def read_data_and_generate_csv(table_name, output_file):
-    try:
-        # Establish a connection to the PostgreSQL database
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            database=DB_NAME,
-            user=DB_USERNAME,
-            password=DB_PASSWORD
-        )
-
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Query to retrieve data for the selected table
-        cursor.execute(f"SELECT start_date, end_date, interval, data FROM {table_name}")
-
-        # Fetch all data
-        data = cursor.fetchall()
-
-        if not data:
-            print("No data found in the selected table.")
-            return
-
-        # Get unique dates from the data
-        unique_dates = set(row[0].date() for row in data)
-
-        if not unique_dates:
-            print("No dates found in the data.")
-            return
-
-        # Display available dates for aggregation
-        print("Available dates for aggregation:")
-        for i, date in enumerate(unique_dates, start=1):
-            print(f"{i}. {date}")
-
-        # Ask the user to select a date
-        try:
-            date_choice = int(input("Enter the number of the date you want to choose for aggregation: "))
-            if 1 <= date_choice <= len(unique_dates):
-                selected_date = list(unique_dates)[date_choice - 1]
-            else:
-                print("Invalid selection.")
-                return
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
-            return
-
-        # Aggregate data for the selected date
-        aggregated_values = aggregate_data_by_date(data, selected_date, aggregation_func=max)
-
-        if not aggregated_values:
-            return
-
-        # Write aggregated data to CSV file
-        write_aggregated_data_to_csv(selected_date, aggregated_values, output_file)
-
-        # Close the cursor and the connection
-        cursor.close()
-        conn.close()
-
-    except Exception as e:
-        print(f"Error: {e}")
--- 69_database_viewer/main.py
+++ 69_database_viewer/main.py
@@ -1,6 +1,40 @@
-from db_reader import read_data_and_generate_csv, select_table
+from db_reader import select_record_and_aggregate, select_table
+import psycopg2
+import sys
+from dotenv import load_dotenv
+import os
 
-selected_table = select_table()
-if selected_table:
-    output_file = input("Enter the name of the output CSV file: ")
-    read_data_and_generate_csv(selected_table, output_file)
+# Load environment variables from .env
+load_dotenv()
+
+# Access the environment variables
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+try:
+    # Establish a connection to the PostgreSQL database
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USERNAME,
+        password=DB_PASSWORD
+    )
+
+    # Call the select_table function to choose a table
+    selected_table = select_table(conn)
+
+    if selected_table:
+        # Call the select_record_and_aggregate function to perform record selection and aggregation
+        select_record_and_aggregate(conn.cursor(), selected_table)
+
+    # Close the connection
+    conn.close()
+
+except Exception as e:
+    # Log the error and display a more informative message
+    error_message = f"An error occurred: {str(e)}"
+    print(error_message)
