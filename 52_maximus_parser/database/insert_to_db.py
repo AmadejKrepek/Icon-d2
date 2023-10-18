dfe0279d898512da3c13ea8ec0cef52631bf0e21
maximuspileus <krepekamadej@gmail.com>
2023-10-18 13:55:19 +0200

    refactored code

--- 52_maximus_parser/database/insert_to_db.py
+++ 52_maximus_parser/database/insert_to_db.py
@@ -58,16 +58,15 @@ def flatten_extend(matrix):
          flat_list.extend(row)
     return flat_list
 
-def insert_parameter_data(provider_id, model_id, parameter_name, data_list, model_run, parameter_table_name):
+def insert_parameter_data(provider_id, model_id, parameter_name, data_list, model_run, parameter_table_name, start_date, end_date):
     try:
-        # Access the environment variables
         DB_USERNAME = os.getenv("DB_USERNAME")
         DB_PASSWORD = os.getenv("DB_PASSWORD")
         DB_HOST = os.getenv("DB_HOST")
         DB_PORT = os.getenv("DB_PORT")
         DB_NAME = os.getenv("DB_NAME")
         logger.info("Establishing a connection to the PostgreSQL database...")
-        # Establish a connection to the PostgreSQL database
+
         conn = psycopg2.connect(
             host=DB_HOST,
             port=DB_PORT,
@@ -78,43 +77,18 @@ def insert_parameter_data(provider_id, model_id, parameter_name, data_list, mode
 
         logger.info("Connection to PostgreSQL established.")
 
-        # Create a cursor object
         cursor = conn.cursor()
 
-        # Initialize combined_data_list as an empty list
         combined_data_list = []
 
-        # Initialize start_date and end_date
-        start_date = None
-        end_date = None            
-
-        local_timezone = pytz.timezone('Europe/Ljubljana')
-
-        # Iterate through data_list and append parameter values for each DataFrame as a nested list
         for data_item in data_list:
-            current_start_date, current_end_date = extract_dates(data_item[0])  # Implement extract_dates function
-
-            df = data_item[0][parameter_name]                
-
             parameter_values = data_item[0][parameter_name].tolist()
             combined_data_list.append(parameter_values)
 
-            # Update start_date with the first start_date in the loop
-            if start_date is None:
-                start_date = current_start_date
-            
-            # Update end_date with the current_end_date in each iteration
-            end_date = current_end_date
-
-        # Add a local timezone (replace 'America/New_York' with your desired timezone)
-        start_date = start_date.tz_localize('UTC').astimezone(local_timezone)
-        end_date = end_date.tz_localize('UTC').astimezone(local_timezone)
-
-        interval = get_interval_from_latest_df(data_list)  # Implement get_interval_from_latest_df function
+        interval = get_interval_from_latest_df(data_list)
         if parameter_name != "2 metre temperature":
             combined_data_list = replaceZeroValuesWithNull(combined_data_list)
 
-        # Insert the entire dataset into the new table with dynamic table name
         insert_data_sql = sql.SQL("""
             INSERT INTO {}
             (provider_id, model_id, model_run, data, start_date, end_date, interval)
