2bac8d7a5ad20459611c0e106d294ee904664a3b
maximuspileus <krepekamadej@gmail.com>
2023-11-15 12:58:54 +0100

    refactored merge lat lon grid

--- 70_genuine_start/main.py
+++ 70_genuine_start/main.py
@@ -1,14 +1,13 @@
+from merger.merge import merge_lat_lon_with_grid_data
 from parse_settings.read.read_colors import read_colors
 import psycopg2
 from psycopg2 import sql
 import os
 from dotenv import load_dotenv
-import csv
 from datetime import datetime, timedelta
 import pandas as pd
 from generate_maps.create import create_maps
 from matplotlib.font_manager import FontProperties
-import pytz
 
 # Load environment variables from .env
 load_dotenv()
@@ -28,38 +27,6 @@ def extract_coordinates(coord_str):
     return lat, lon
 
 
-def get_latitudes_and_longitudes(provider_id, model_id):
-    try:
-        # Establish a connection to the PostgreSQL database
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            database=DB_NAME,
-            user=DB_USERNAME,
-            password=DB_PASSWORD
-        )
-
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Query the latitudes and longitudes based on provider_id and model_id from the lat_lon_schema table
-        cursor.execute("SELECT latitudes, longitudes FROM lat_lon_schema WHERE provider_id = %s AND model_id = %s",
-                       (provider_id, model_id))
-
-        # Fetch the row
-        row = cursor.fetchone()
-
-        if row:
-            latitudes, longitudes = row
-            return latitudes, longitudes
-        else:
-            print(f"No latitudes and longitudes found for provider_id {provider_id} and model_id {model_id}.")
-            return None, None
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return None, None
-
 
 def write_data_to_csv_with_coordinates(selected_start_date, selected_end_date, selected_model_run, date_choice,
                                        table_name, output_file, provider_id, model_id):
@@ -73,88 +40,25 @@ def write_data_to_csv_with_coordinates(selected_start_date, selected_end_date, s
             password=DB_PASSWORD
         )
 
-        # Create a cursor object
-        cursor = conn.cursor()
+        csv_data = merge_lat_lon_with_grid_data(conn, table_name, selected_start_date, selected_end_date, selected_model_run, provider_id, model_id)
 
-        # Define the table name as an SQL Identifier
-        table_identifier = sql.Identifier(table_name)
-
-        # Query the data from the specified table
-        # Use the table_identifier in the SQL query
-        query = sql.SQL("SELECT start_date, end_date, interval, model_run, data FROM {} "
-                        "WHERE start_date = %s AND end_date = %s AND model_run = %s").format(table_identifier)
-
-        # Execute the SQL query with the provided parameters
-        cursor.execute(query, (selected_start_date, selected_end_date, selected_model_run))
-
-        # Fetch all rows
-        rows = cursor.fetchall()
-
-        csv_data = []
-        init_interval = None
-        latitudes, longitudes = get_latitudes_and_longitudes(provider_id, model_id)
-
-        if rows:
-            for row in rows:
-                start_date, end_date, interval, model_run, data = row
-                # Convert start_date and end_date to the Slovenia (Europe/Ljubljana) timezone
-                start_date = start_date.astimezone(pytz.timezone('Europe/Ljubljana'))
-                end_date = end_date.astimezone(pytz.timezone('Europe/Ljubljana'))
-
-                # Check if the interval is 2 hours and adjust it to 1 hour if necessary.
-                if interval == timedelta(hours=2):
-                    interval = timedelta(hours=1)
-
-                current_date = start_date
-                interval_seconds = int(interval.total_seconds())
-
-                if init_interval is None:
-                    init_interval = interval
-
-                for day_data in data:
-                    current_date += timedelta(seconds=interval_seconds)
-
-                    # Replace None with 0.0 in weather data
-                    day_data = [0.0 if value is None else value for value in day_data]
-
-                    # Initialize an index to track the current coordinate
-                    coordinate_index = 0
-
-                    for value in day_data:
-                        # Ensure the coordinate index stays within bounds
-                        coordinate_index %= len(latitudes)
-
-                        # Get the latitude and longitude for the current index
-                        lat = latitudes[coordinate_index]
-                        lon = longitudes[coordinate_index]
-
-                        # Combine timestamp, weather data, and coordinates
-                        combined_data = (current_date, value, lat, lon)
-
-                        # Append the combined data point to the CSV data
-                        csv_data.append(combined_data)
-
-                        # Increment the coordinate index
-                        coordinate_index += 1
-
-            agg_name = agg_function + '_' + table_name
-
-            df = pd.DataFrame(csv_data, columns=['Datetime', agg_name, 'Latitude', 'Longitude'])
-
-            df, selected_date = filterSpecificDate(df, date_choice, end_date)
-
-            df = convert_data(df, agg_name)
-
-            df = createAgregates(df, agg_function, table_name)
-
-            return df, selected_date
-
-        else:
+        if csv_data is None:
             print(f"No data found in table '{table_name}'.")
+            return Exception("Wrong")
 
-        # Close the cursor and the connection
-        cursor.close()
+        agg_name = agg_function + '_' + table_name
+
+        df = pd.DataFrame(csv_data, columns=['Datetime', agg_name, 'Latitude', 'Longitude'])
+
+        df, selected_date = filterSpecificDate(df, date_choice, end_date)
+
+        df = convert_data(df, agg_name)
+
+        df = createAgregates(df, agg_function, table_name)
+
         conn.close()
+
+        return df, selected_date
 
     except Exception as e:
         print(f"Error: {e}")
---
+++ 70_genuine_start/merger/merge.py
@@ -0,0 +1,79 @@
+from datetime import timedelta
+
+import pytz
+from psycopg2 import sql
+
+from merger.latitude import get_latitudes_and_longitudes
+
+
+def merge_lat_lon_with_grid_data(conn, table_name, selected_start_date, selected_end_date, selected_model_run, provider_id, model_id):
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Define the table name as an SQL Identifier
+    table_identifier = sql.Identifier(table_name)
+
+    # Query the data from the specified table
+    # Use the table_identifier in the SQL query
+    query = sql.SQL("SELECT start_date, end_date, interval, model_run, data FROM {} "
+                    "WHERE start_date = %s AND end_date = %s AND model_run = %s").format(table_identifier)
+
+    # Execute the SQL query with the provided parameters
+    cursor.execute(query, (selected_start_date, selected_end_date, selected_model_run))
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    csv_data = []
+    init_interval = None
+    latitudes, longitudes = get_latitudes_and_longitudes(provider_id, model_id, conn)
+
+    if rows:
+        for row in rows:
+            start_date, end_date, interval, model_run, data = row
+            # Convert start_date and end_date to the Slovenia (Europe/Ljubljana) timezone
+            start_date = start_date.astimezone(pytz.timezone('Europe/Ljubljana'))
+            end_date = end_date.astimezone(pytz.timezone('Europe/Ljubljana'))
+
+            # Check if the interval is 2 hours and adjust it to 1 hour if necessary.
+            if interval == timedelta(hours=2):
+                interval = timedelta(hours=1)
+
+            current_date = start_date
+            interval_seconds = int(interval.total_seconds())
+
+            if init_interval is None:
+                init_interval = interval
+
+            for day_data in data:
+                current_date += timedelta(seconds=interval_seconds)
+
+                # Replace None with 0.0 in weather data
+                day_data = [0.0 if value is None else value for value in day_data]
+
+                # Initialize an index to track the current coordinate
+                coordinate_index = 0
+
+                for value in day_data:
+                    # Ensure the coordinate index stays within bounds
+                    coordinate_index %= len(latitudes)
+
+                    # Get the latitude and longitude for the current index
+                    lat = latitudes[coordinate_index]
+                    lon = longitudes[coordinate_index]
+
+                    # Combine timestamp, weather data, and coordinates
+                    combined_data = (current_date, value, lat, lon)
+
+                    # Append the combined data point to the CSV data
+                    csv_data.append(combined_data)
+
+                    # Increment the coordinate index
+                    coordinate_index += 1
+        # Close the cursor and the connection
+        cursor.close()
+        return csv_data
+    else:
+        # Close the cursor and the connection
+        cursor.close()
+        return None
