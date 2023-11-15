e4e0304c94e5ad050edc0a74a22097908bd2fd13
maximuspileus <krepekamadej@gmail.com>
2023-11-15 14:41:37 +0100

    showed only full hours with data without empty once

--- 70_genuine_start/animation/animation.py
+++ 70_genuine_start/animation/animation.py
@@ -4,11 +4,14 @@ from matplotlib.animation import FuncAnimation
 
 
 def create_frames(df, agg_name):
-    # Assuming your DataFrame has a 'Timestamp' column
+    # Assuming your DataFrame has a 'Datetime' column
     df['Datetime'] = pd.to_datetime(df['Datetime'])
 
     # Sort the DataFrame by timestamp
     df = df.sort_values(by='Datetime')
+
+    # Extract unique hours from the 'Datetime' column
+    unique_hours = df['Datetime'].dt.hour.unique()
 
     # Create a figure and axis for the plot
     fig, ax = plt.subplots()
@@ -22,8 +25,8 @@ def create_frames(df, agg_name):
         ax.scatter(hour_df['Longitude'], hour_df['Latitude'], c=hour_df[agg_name], cmap='viridis')
         ax.set_title(f'Hour {frame}')
 
-    # Set the range of frames (hours in a day)
-    frames_range = range(24)
+    # Set the range of frames (unique hours in the 'Datetime' column)
+    frames_range = unique_hours
 
     # Create the animation
     animation = FuncAnimation(fig, update, frames=frames_range, repeat=True)
