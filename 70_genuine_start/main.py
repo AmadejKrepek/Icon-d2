a5ace4f10f277328ebf76c5c0e24c18163032839
maximuspileus <krepekamadej@gmail.com>
2024-02-08 20:20:21 +0100

    updated conversion unmits

--- 70_genuine_start/main.py
+++ 70_genuine_start/main.py
@@ -115,13 +115,16 @@ def createAgregates(df, agg_function, table_name):
 def convert_ms_to_kmh(ms):
     return ms * 3.6
 
+def convert_m_to_cm(value):
+    return value * 100;
+
 
 def convert_data(df, table_name):
     if (table_name.startswith('max_10_metre_v_wind_component_icond2') or table_name.startswith(
-            'max_maximum_wind_10m_icond2')):
-        # Access the third column (index 2) and apply the conversion function
-        print('Converted kmh to ms...')
+            'max_maximum_wind_10m_icond2') or table_name.startswith('animation_maximum_wind_10m_icond2')):
         df[table_name] = df[table_name].apply(convert_ms_to_kmh)
+    elif (table_name.startswith('max_snow_depth_icond2') or table_name.startswith('sum_snow_depth_icond2')):
+        df[table_name] = df[table_name].apply(convert_m_to_cm)
 
     return df
 
