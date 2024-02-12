634dd5b404b980064eff40b6ae0bf40f4fd4aece
maximuspileus <krepekamadej@gmail.com>
2024-02-12 04:03:35 +0100

    created cache

--- 84_web_api/background/task.py
+++ 84_web_api/background/task.py
@@ -3,27 +3,52 @@
 from datetime import datetime, timedelta
 
 
-def generate_cache_key(parameter, day, agg, start_date):
-    return f'weather_data_{parameter}_{day}_{agg}_{start_date}'
+def generate_cache_key(parameter):
+    return f'{parameter}'
 
 
 def store_last_records(parameter, day, agg, start_date, img_io, cache):
-    cache_key = generate_cache_key(parameter, day, agg, start_date)
+    cache_key = generate_cache_key(parameter)
 
     # Retrieve the current cache records or initialize an empty list
     current_records = cache.get(cache_key) or []
 
-    # Remove records older than 2 days
-    current_records = [record for record in current_records if
-                       datetime.now() - record['timestamp'] <= timedelta(days=2)]
+    # Check if the new data already exists in the records
+    new_data_exists = any(
+        record['start_date'] == start_date
+        and record['parameter'] == parameter
+        and record['day'] == day
+        and record['agg'] == agg
+        for record in current_records)
 
-    # Append the new record to the list
-    current_records.append({'timestamp': datetime.now(), 'data': img_io})
+    if not new_data_exists:
+        # Remove records older than 2 days
+        current_records = [record for record in current_records if
+                           datetime.now() - record['timestamp'] <= timedelta(days=2)]
 
-    # Save the updated list in the cache
-    cache.set(cache_key, current_records, timeout=None)  # No need to set a timeout
+        # Append the new record to the list
+        current_records.append({'timestamp': datetime.now(), 'data': img_io, 'start_date': start_date,
+                                'day': day, 'parameter': parameter, 'agg': agg})
+
+        # Save the updated list in the cache
+        cache.set(cache_key, current_records, timeout=None)  # No need to set a timeout
 
 
-def get_last_records(parameter, day, agg, start_date, cache):
-    cache_key = generate_cache_key(parameter, day, agg, start_date)
-    return cache.get(cache_key) or []
+def get_last_records(parameter, start_date, day, agg, cache):
+    cache_key = generate_cache_key(parameter)
+    all_records = cache.get(cache_key) or []
+
+    # Find the first record that matches all specified conditions
+    matching_record = next(
+        (record for record in all_records
+         if record.get('parameter') == parameter
+            and record.get('start_date') == start_date
+            and record.get('day') == int(day)
+            and record.get('agg') == agg),
+        None  # Default value if no matching record is found
+    )
+
+    return [matching_record] if matching_record else []
+
+
+
--- 84_web_api/main.py
+++ 84_web_api/main.py
@@ -3,11 +3,13 @@ import os
 import threading
 import time
 import multiprocessing
+import matplotlib
+
+matplotlib.use('Agg')
 
 import schedule
 from flask import Flask, jsonify, request, send_file
 from flask_caching import Cache
-from matplotlib.font_manager import FontProperties
 
 from assets.parameters.parameters import parameters
 from background.proceed import run_background_task
@@ -19,8 +21,10 @@ from features.parse_settings.read.read_colors import read_colors
 from utils.processor import process_last_images
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-
+# Configure Flask-Caching to use a disk-based cache
+app.config['CACHE_TYPE'] = 'filesystem'
+app.config['CACHE_DIR'] = 'cache'
+cache = Cache(app)
 # Define the cache timeout (in seconds), for example, set to 1 hour
 CACHE_TIMEOUT = 3600
 
@@ -34,7 +38,7 @@ def get_weather():
     day = request.args.get('day')
     agg = request.args.get('agg')
     start_date = request.args.get('start_date')
-    cached_data = get_last_records(parameter, day, agg, start_date, cache)
+    cached_data = get_last_records(parameter, start_date, day, agg, cache)
     if cached_data:
         # Return the cached data as a response
         return send_file(cached_data[0]['data'], mimetype='image/png')
@@ -49,17 +53,13 @@ def get_weather():
 if __name__ == '__main__':
     # Loop through parameters, days, and aggregation values for icon_d2 and aladin separately
     for parameter in [param for param in parameters]:
-            for agg in ["max"]:  # Aggregation values
-                for day in range(1, 2):  # Days from 1 to 3 for icon_d2
-                    start_date = "2024-02-08 18:00:00"  # Your specific start_date
-                    #background_thread = threading.Thread(target=run_background_task,
-                                                         #args=(parameter, day, agg, start_date, cache))
-                    #background_thread.start()
-                    #background_process = multiprocessing.Process(target=run_background_task, args=(
-                    #parameter, day, agg, start_date, cache))
-                    #background_process.start()
-                    run_background_task(parameter, day, agg, start_date, cache)
-                break
-            break
+        for agg in ["max", "min"]:  # Aggregation values
+            for day in range(1, 4):  # Days from 1 to 3 for icon_d2
+                start_date = "2024-02-08 18:00:00"  # Your specific start_date
+                # background_process = multiprocessing.Process(target=run_background_task, args=(
+                # parameter, day, agg, start_date, cache))
+                # background_process.start()
+                # background_process.join()
+                run_background_task(parameter, day, agg, start_date, cache)
 
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
